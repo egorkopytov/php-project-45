1ea9e534289a6353d604c748a3c0e16cb94a9f2a
egor kopytov <egor.kopytoff>
2025-02-28 13:50:56 +0800

    add code climate

---
+++ src/Cli.php
@@ -0,0 +1,14 @@
+#!/usr/bin/env php
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use function cli\line;
+use function cli\prompt;
+
+function greetings() {
+
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
+
+}
