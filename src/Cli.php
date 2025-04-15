adf0083c274043bb3c40012d74003acb67717264
egor kopytov <egor.kopytoff>
2025-04-15 14:43:45 +0800

    fix

--- src/Cli.php
+++ src/Cli.php
@@ -1,6 +1,15 @@
 #!/usr/bin/env php
 <?php
 require __DIR__ . '/../vendor/autoload.php';
-require __DIR__ . '/../src/greetings.php';
 
-greetings();
+
+use function cli\line;
+use function cli\prompt;
+
+function greetings() 
+{
+    echo "Welcome to the Brain Games!\n";
+    $name = prompt('May I have your name?');
+    line("Hello, {$name}!");
+    //return $name;
+}
--- src/Enginge.php
+++ src/Enginge.php
@@ -1,11 +1,14 @@
+#!/usr/bin/env php
 <?php
+require __DIR__ . '/../vendor/autoload.php';
 
 use function cli\line;
 use function cli\prompt;
 
-function greetings()
+function Greetings()
 {
-    line('Welcome to the Brain Game!');
-    $name = prompt('May I have your name?');
-    line("Hello, %s!", $name);
+        line('Welcome to the Brain Game!');
+        $name = prompt('May I have your name?');
+        line("Hello, %s!", $name);
 }
+
