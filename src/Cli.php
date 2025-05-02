600bd637295d9a3a5e1ce74119739be8ce64c83c
egor kopytov <egor.kopytoff>
2025-05-02 23:11:56 +0800

    fix cli.php

--- src/Cli.php
+++ src/Cli.php
@@ -3,13 +3,6 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 
-use function cli\line;
-use function cli\prompt;
-
-function greetings() 
-{
-    echo "Welcome to the Brain Games!\n";
-    $name = prompt('May I have your name?');
-    line("Hello, {$name}!");
-    //return $name;
+function greetUser() {
+    echo "Welcome to Brain Games!\n";
 }
