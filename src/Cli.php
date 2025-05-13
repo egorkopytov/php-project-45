21ce2faa5950f35238d406f96e2f9753abd47cba
egor kopytov <egor.kopytoff>
2025-05-13 09:09:01 +0800

    add new

--- src/Cli.php
+++ src/Cli.php
@@ -4,4 +4,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 function greetUser() {
     echo "Welcome to the Brain Games!\n";
+    echo "May I have your name?\n";
+    $name = trim(fgets(STDIN));
+    echo "Hello, {$name}!\n";
 }
